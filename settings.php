02140deb73c538f23dede3606b50b4ed15b763c4
Steve Hearnden <steve@hearnden.org.uk>
2014-01-22 01:14:25 +0000

    Added settings

---
+++ settings.php
@@ -0,0 +1,5 @@
+<?php
+$netrail_user = '*****@******.org.uk';
+$netrail_password = '***********';
+$website_post = 'http://hearnden.org.uk/traintrack/update_from_remote.php';
+?>
---
+++ settings_server.php
@@ -0,0 +1,5 @@
+<?php
+$sql_user='*********';
+$sql_pass='***********';
+$sql_dsn='mysql:dbname=**************;host=127.0.0.1';
+?>
